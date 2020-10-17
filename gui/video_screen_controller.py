33b3a3618801a4ec14547ea93615d9202ce272f7
Irrmm <grazashar@mail.ru>
2020-10-17 17:12:40 +0200

    Add draft structure

---
+++ gui/video_screen_controller.py
@@ -0,0 +1,3 @@
+class VideoScreenController:
+    def __init__(self):
+        pass
---
+++ sensor/video.py
@@ -0,0 +1,12 @@
+class VideoStreamer:
+    def __init__(self):
+        pass
+
+    def stream(self):
+        pass
+
+    def run(self):
+        pass
+
+    def stop(self):
+        pass
