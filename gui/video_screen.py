ea429d22cbcb9fb29b97391959d59f48f3cbc353
Irrmm <grazashar@mail.ru>
2020-10-21 21:29:21 +0200

    Enable read video stream

--- gui/video_screen.py
+++ gui/video_screen.py
@@ -1,10 +1,15 @@
 from PyQt5 import QtWidgets
+from gui.video_screen_controller import VideoScreenController
 
 
 class VideoScreen(QtWidgets.QWidget):
     def __init__(self, title: str):
         super().__init__()
         self.setWindowTitle(title)
+        self._init_gui()
+        self._create_controller()
+
+    def _init_gui(self):
         available_geometry = QtWidgets.QDesktopWidget().availableGeometry()
         screen_center = available_geometry.center()
 
@@ -16,3 +21,9 @@ class VideoScreen(QtWidgets.QWidget):
 
         self.setGeometry(x, y, width, height)
 
+    def _create_controller(self):
+        self._controller = VideoScreenController()
+        self._controller.read_frame.connect(self._on_frame_read)
+
+    def _on_frame_read(self, frame):
+        print(frame)
--- gui/video_screen_controller.py
+++ gui/video_screen_controller.py
@@ -2,18 +2,34 @@ from PyQt5 import QtCore
 from sensor.video import VideoFrameReader
 
 
-class VideoScreenController:
+class VideoScreenController(QtCore.QObject):
+    read_frame = QtCore.pyqtSignal(object)
+
     def __init__(self):
-        pass
+        super().__init__()
+        self._create_and_start_streamer()
+
+    def _create_and_start_streamer(self):
+        self._thread = QtCore.QThread()
+        self._streamer = VideoStreamer()
+        self._streamer.moveToThread(self._thread)
+        self._streamer.read_frame.connect(self._on_frame_read)
+        self._thread.started.connect(self._streamer.run)
+        self._thread.start()
+
+    def _on_frame_read(self, frame):
+        self.read_frame.emit(frame)
 
 
 class VideoStreamer(QtCore.QObject):
+    read_frame = QtCore.pyqtSignal(object)
+
     def __init__(self):
-        self.frame_reader = VideoFrameReader(device=0)
+        super().__init__()
+        self._frame_reader = VideoFrameReader(device=0)
 
     def run(self):
         while True:
-            image = self.frame_reader.read()
-            if image:
-                # send signal
-                pass
+            image = self._frame_reader.read()
+            if image is not None:
+                self.read_frame.emit(image)
