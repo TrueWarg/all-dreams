f275c789b0104550e7d24973820605e2cf4c4919
Irrmm <grazashar@mail.ru>
2020-11-03 21:58:59 +0200

    Add draft video<->instrument panel layout

--- gui/edge_based_dices_detection_screen.py
+++ gui/edge_based_dices_detection_screen.py
@@ -17,21 +17,41 @@ class EdgeBasedDicesDetectionScreen(QtWidgets.QWidget):
         screen_center = available_geometry.center()
 
         self.width = available_geometry.width() // 2
-        self.height = available_geometry.height() // 1.5
+        self.height = available_geometry.height() // 3
 
         x = screen_center.x() - self.width // 2
         y = screen_center.y() - self.height // 2
 
         self.setGeometry(x, y, self.width, self.height)
         self.image = QtWidgets.QLabel(self)
-        self.image.resize(self.width, self.height)
+        self.image.resize(self.width // 2, self.height)
+        # temporary, remove later
+        self.test_slider1 = QtWidgets.QSlider(Qt.Horizontal, self)
+        self.test_slider1.setMaximumWidth(self.width // 2)
+        self.test_slider2 = QtWidgets.QSlider(Qt.Horizontal, self)
+        self.test_slider2.setMaximumWidth(self.width // 2)
+        self.test_slider3 = QtWidgets.QSlider(Qt.Horizontal, self)
+        self.test_slider3.setMaximumWidth(self.width // 2)
+
+        main_h_box = QtWidgets.QHBoxLayout()
+
+        main_h_box.addWidget(self.image)
+
+        instrument_panel_v_box = QtWidgets.QVBoxLayout()
+        instrument_panel_v_box.addWidget(self.test_slider1)
+        instrument_panel_v_box.addWidget(self.test_slider2)
+        instrument_panel_v_box.addWidget(self.test_slider3)
+
+        main_h_box.addLayout(instrument_panel_v_box)
+
+        self.setLayout(main_h_box)
 
     def _create_controller(self):
         self._controller = EdgeBasedDicesDetectionController()
         self._controller.images.connect(self._on_image_received)
 
     def _on_image_received(self, image):
-        scaled = image.scaled(self.width, self.height, Qt.KeepAspectRatio)
+        scaled = image.scaled(self.width // 2, self.height, Qt.KeepAspectRatio)
         pix_map = QPixmap.fromImage(scaled)
         self.image.setPixmap(pix_map)
 
