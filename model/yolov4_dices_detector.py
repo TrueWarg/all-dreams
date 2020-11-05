2d41a3317876bb27d8b4ed2c40952c88afa99964
Irrmm <grazashar@mail.ru>
2020-11-05 21:31:41 +0200

    Impl YoloV4DicesDetector using dnn opencv module

--- model/yolov4_dices_detector.py
+++ model/yolov4_dices_detector.py
@@ -1,12 +1,34 @@
-# todo try different ways:
-# 1. Use cv2.dnn for inference with cuda backand;
-# 2. darknet framework directly;
-# 3. some PyTorch wrapper;
-# 4. ???
-# Explore methods for detect angle of dices rotations later (like in edge based)
-class YoloV4DicesDetector:
-    def __init__(self):
-        pass
+import cv2
+import numpy as np
+from dataclasses import dataclass
 
-    def detect(self):
-        pass
+
+@dataclass
+class Config:
+    backend: str
+    target: str
+    img_scale_factor: float
+    img_size: tuple
+
+
+class YoloV4DicesDetector:
+    def __init__(self,
+                 weight_path: str,
+                 config_path: str,
+                 config: Config
+            ):
+        net = cv2.dnn.readNet(weight_path, config_path)
+        net.setPreferableBackend(config.backend)
+        net.setPreferableTarget(config.target)
+        self.config = config
+        self.net = net
+
+    def detect(self, img: np.array):
+        blob = cv2.dnn.blobFromImage(
+            img,
+            scalefactor=self.config.img_scale_factor,
+            size=self.config.img_size,
+            swapRB=True
+        )
+        self.net.setInput(blob)
+        predictions = self.net.forward(self.net.getUnconnectedOutLayersNames())
